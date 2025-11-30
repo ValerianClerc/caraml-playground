12eb42da277f26934a3fca0ce5ebb1f8541950b6
Valerian Clerc <valerian.clerc@gmail.com>
2025-11-29 17:21:21 -0800

    Add CompilationTab, add disabled states and auto-resetting tabs to ExecSidePanel

--- client/src/components/side-panel/CompilationTab.tsx
+++ client/src/components/side-panel/CompilationTab.tsx
@@ -1,11 +1,33 @@
 import { selectCurrentRun, useAppState } from "@/state";
+import { Text } from "@/components/retroui/Text";
+import { Button } from "@/components/retroui/Button";
 
 export const CompilationTab = () => {
   const currentRun = useAppState(selectCurrentRun)
 
-  if (!currentRun) return <p>No run selected. Compile new code, or view an existing run.</p>;
-  if (currentRun?.status === "failed") return <p>Compilation failed with error: {currentRun?.errorMessage}</p>;
-  if (currentRun?.status !== "succeeded") return <p>Compilation status is "{currentRun?.status}". Executable artifacts are only available for succeeded runs.</p>;
+  let message = '';
+  let statusColor = '';
+  if (!currentRun) {
+    message = 'No run selected. Compile new code, or view an existing run.';
+  } else if (currentRun?.status === "failed") {
+    message = `Compilation failed with error: ${currentRun?.errorMessage}`;
+    statusColor = 'text-red-600';
+  } else if (currentRun?.status !== "succeeded") {
+    message = `Executable artifacts are only available for succeeded runs.`;
+    statusColor = 'text-yellow-600';
+  } else if (currentRun?.status === "succeeded") {
+    statusColor = 'text-green-700';
+  }
 
-  return <p>Compilation succeeded!</p>
+  return <div className="gap-2 flex flex-col">
+    <Text>Compilation status: <span className={`font-semibold ${statusColor}`}>{currentRun?.status}</span></Text>
+    <Text>{message}</Text>
+
+    {currentRun?.code && (
+      <>
+        <Text as="h5">Source code:</Text>
+        <code className="block whitespace-pre-wrap font-mono text-sm border-2 border-border rounded p-2">{currentRun?.code.trim()}</code>
+      </>
+    )}
+  </div>
 }
--- client/src/components/side-panel/ExecSidePanel.tsx
+++ client/src/components/side-panel/ExecSidePanel.tsx
@@ -1,18 +1,36 @@
+import { selectCurrentRun, useAppState } from "@/state";
 import { Card } from "../retroui/Card";
 import { Tabs, TabsContent, TabsPanels, TabsTrigger, TabsTriggerList } from "../retroui/Tab";
+import { CompilationTab } from "./CompilationTab";
+import { useEffect, useState } from "react";
 
 export const ExecSidePanel = () => {
+  const currentRun = useAppState(selectCurrentRun)
+  const [selectedTabIndex, setSelectedTabIndex] = useState<number | undefined>(0);
+
+  useEffect(() => {
+    // if current tab is IR or Execution, and currentRun becomes invalid, switch to Compilation tab
+    if (selectedTabIndex && selectedTabIndex > 0) {
+      if (!currentRun || currentRun.status !== "succeeded") {
+        setSelectedTabIndex(0);
+      }
+    }
+  }, [currentRun])
+
+  const shouldDisableIRandExecTabs = !currentRun || currentRun.status !== "succeeded";
+  const disabledText = "Must successfully compile code to enable this tab.";
+
   return (
     <Card>
-      <Tabs>
+      <Tabs selectedIndex={selectedTabIndex} onChange={setSelectedTabIndex} >
         <TabsTriggerList className="w-full">
-          <TabsTrigger className="flex-1 min-w-0 truncate">Compilation</TabsTrigger>
-          <TabsTrigger className="flex-1 min-w-0 truncate">IR</TabsTrigger>
-          <TabsTrigger className="flex-1 min-w-0 truncate">Execution</TabsTrigger>
+          <TabsTrigger className="flex-1 min-w-0 truncate" >Compilation</TabsTrigger>
+          <TabsTrigger className="flex-1 min-w-0 truncate" disabled={shouldDisableIRandExecTabs} disabledText={disabledText}>IR</TabsTrigger>
+          <TabsTrigger className="flex-1 min-w-0 truncate" disabled={shouldDisableIRandExecTabs} disabledText={disabledText}>Execution</TabsTrigger>
         </TabsTriggerList>
         <TabsPanels>
           <TabsContent>
-            Compilation tab
+            <CompilationTab />
           </TabsContent>
           <TabsContent>
             IR tab
